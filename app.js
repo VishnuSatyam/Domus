cdd6813ac3edb60494e1359a12e8a53e36f05617
Vishnu Satyam <pandeyvishnusatyam@gmail.com>
2026-06-24 07:00:20 +0530

    added some new features

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+app.engine("ejs" , ejsMate);
 
 main()
 .then(()=>{
@@ -20,6 +24,8 @@ app.set("view engine" , "ejs");
 
 app.set("views" , path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname , "public")));
 
 app.get("/", (req , res)=>{
     res.send("root is working")
@@ -30,11 +36,46 @@ app.get("/listings" , async (req , res)=>{
     res.render("listings/index",{allListings});
 });
 
+// new route
+
+app.get("/listings/new" , (req , res)=>{
+    res.render("listings/new");
+});
+
 // show route
 app.get("/listings/:id" , async(req , res)=>{
     const {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show" , {listing});
+});
+
+// create route
+app.post("/listings" , async(req , res)=>{
+    const newListing = new Listing(req.body);
+    await newListing.save();
+    res.redirect(`/listings/${newListing._id}`);
+});
+
+// edit route
+app.get("/listings/:id/edit" , async(req , res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit" , {listing});
+});
+
+// update route
+
+app.put("/listings/:id" , async (req , res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
+
+// delete route
+app.delete("/listings/:id" , async (req , res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
 });
 
 // app.get("/testListing" , async(req , res)=>{
